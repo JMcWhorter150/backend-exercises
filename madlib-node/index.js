0d9e92c665cfa8c6e8991433af53f13cd7863bcc
Joe McWhorter <joseph.s.mcwhorter@gmail.com>
2019-12-19 14:18:04 -0500

    Adds bonus exercise

--- madlib-node/index.js
+++ madlib-node/index.js
@@ -1,3 +1,4 @@
+const fs = require('fs');
 const catNames = require('cat-names');
 const supervillains = require('supervillains');
 const superb = require('superb');
@@ -7,5 +8,11 @@ const catName = catNames.random();
 const supervillain = supervillains.random();
 const superbAdj = superb.random();
 
-const sentence = petDefense(catName, supervillain, superbAdj);
-console.log(sentence);
+const sentence = petDefense(catName, supervillain, superbAdj) + `\n`;
+// console.log(sentence);
+
+fs.writeFile('madlibs.txt', sentence, {flag: "a"}, err => {
+    if (err) {console.log(err);} else {
+        console.log("File saved!");
+    }
+});
